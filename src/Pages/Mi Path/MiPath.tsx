59006840d6b501da72fc6fd5b954446daf85e78f
Juan Pablo Castaño <jpc4stano@gmail.com>
2025-03-12 14:26:56 -0500

    MyPath valida el mínimo de horas y se ve mas bonito

--- src/Pages/Mi Path/MiPath.tsx
+++ src/Pages/Mi Path/MiPath.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AppStore } from "../../Redux/store";
 
 import { useDispatch } from "react-redux";
@@ -21,6 +21,9 @@ const MiPath = () => {
   const dispatch = useDispatch();
   const userState = useSelector((store: AppStore) => store.user);
   const [activities, setActivities] = useState<Activity[]>(path.activities);
+  const today = new Date().toISOString().split("T")[0];
+  const [totalHours, setTotalHours] = useState(0)
+  const [minHours, _] = useState(import.meta.env.VITE_MIN_HOURS|36)
 
   function generateUUID() {
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
@@ -43,13 +46,16 @@ const MiPath = () => {
     console.log(activities);
   };
 
+  useEffect(()=>{
+    setTotalHours(activities.reduce((sum, activity) => sum + activity.hours, 0))
+  },[activities])
+
   if (!userState.pathId) {
     return (
       <>
         <div className={`${styles.container} ${styles.mainContainer} `}>
-          <p className={styles.label}>
-            Parece ser que no tienes un path registrado, crea uno y añade
-            actividades
+          <p className={styles.info}>
+            Parece que aún no tienes un path registrado
           </p>
           <button
             onClick={() => {
@@ -68,78 +74,95 @@ const MiPath = () => {
         <div className={styles.mainContainer}>
           <div className={styles.title}>
             <h1>{path.name}</h1>
+            <p>Horas Totales: {totalHours}/{minHours}</p>
           </div>
+          <button
+            onClick={() => {
+              crearPath();
+            }}
+            className={`${styles.button} ${totalHours>=minHours?'dark-gradient-primary':styles.inactive}`}
+          >
+            <p className={styles.text}>{totalHours>=minHours?'Enviar path a mi coach':'Completa el mínimo de horas'}</p>
+          </button>
+        </div>
 
-          <hr />
+        {activities.map((Activity, key) => {
+          let ParsedKey = key + 1;
+          return (
+            <div className={styles.mainContainer}>
+              <h2 className={styles.noMargin}>{"Actividad " + ParsedKey + " "}</h2>
+              <h2>{Activity.name}</h2>
+              <p>{Activity.description}</p>
+              <p>{"Horas Necesarias: " + Activity.hours}</p>
+              <p>{"Presupuesto: $" + Activity.budget}</p>
+              <p>{"Fecha De Inicio: " + Activity.initialDate}</p>
+              <p>{"Fecha De Finalización: " + Activity.finalDate}</p>
+              <p>
+                <strong>{"Estado: " + Activity.state}</strong>
+              </p>
+            </div>
+          );
+        })}
+        <div className={`${styles.activityform} ${styles.mainContainer}`}>
+          <h2 className={styles.noMargin}>Añadir Actividad</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const formData = new FormData(e.currentTarget);
+              const newActivity: Activity = {
+                id: generateUUID(),
+                name: formData.get("name") as string,
+                description: formData.get("description") as string,
+                hours: Number(formData.get("hours")),
+                initialDate: new Date(formData.get("initialDate") as string),
+                finalDate: new Date(formData.get("finalDate") as string),
+                budget: Number(formData.get("budget")),
+                state: "Pendiente",
+                pathId: userState.pathId,
+              };
+              addActivity(newActivity);
+              e.currentTarget.reset();
+            }}
+          >
+            <div className={styles.formGroup}>
+              <label htmlFor="name">Nombre:</label>
+              <input type="text" id="name" name="name" required />
+            </div>
 
-          {activities.map((Activity, key) => {
-            let ParsedKey = key+1
-            return (
-              <div className={styles.activityContainer}>
-                <h1>{"Actividad " + ParsedKey + " "}</h1>
-                <h1>{Activity.name}</h1>
-                <p>{"Descripción: " + Activity.description}</p>
-                <p>{"Horas Necesarias: " + Activity.hours}</p>
-                <p>{"Presupuesto: " + Activity.budget}</p>
-                <p>{"Fecha De Inicio: " + Activity.initialDate}</p>
-                <p>{"Fecha De Finalización: " + Activity.finalDate}</p>
-                <p>
-                  <strong>{"Estado: " + Activity.state}</strong>
-                </p>
-              </div>
-            );
-          })}
+            <div className={styles.formGroup}>
+              <label htmlFor="description">Descripción:</label>
+              <input id="description" name="description" required />
+            </div>
 
-          <hr />
-          <div className={styles.activityform}>
-            <h2>Añadir Actividad</h2>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                const formData = new FormData(e.currentTarget);
-                const newActivity: Activity = {
-                  id: generateUUID(),
-                  name: formData.get("name") as string,
-                  description: formData.get("description") as string,
-                  hours: Number(formData.get("hours")),
-                  initialDate: new Date(formData.get("initialDate") as string),
-                  finalDate: new Date(formData.get("finalDate") as string),
-                  budget: Number(formData.get("budget")),
-                  state: formData.get("state") as string,
-                  pathId: userState.pathId,
-                };
-                addActivity(newActivity);
-                e.currentTarget.reset();
-              }}
-            >
-              <div className={styles.formGroup}>
-                <label htmlFor="name">Nombre:</label>
-                <input type="text" id="name" name="name" required />
-              </div>
-
-              <div className={styles.formGroup}>
-                <label htmlFor="description">Descripción:</label>
-                <input id="description" name="description" required />
-              </div>
-
-              <div className={styles.formGroup}>
-                <label htmlFor="hours">Horas:</label>
-                <input type="number" id="hours" name="hours" min="0" required />
-              </div>
-
+            <div className={styles.formRow}>
               <div className={styles.formGroup}>
                 <label htmlFor="initialDate">Fecha Inicial:</label>
                 <input
+                  className={styles.activityInputDate}
                   type="date"
                   id="initialDate"
                   name="initialDate"
+                  min={today}
                   required
                 />
               </div>
 
               <div className={styles.formGroup}>
                 <label htmlFor="finalDate">Fecha Final:</label>
-                <input type="date" id="finalDate" name="finalDate" required />
+                <input
+                  className={styles.activityInputDate}
+                  type="date"
+                  id="finalDate"
+                  name="finalDate"
+                  min={today}
+                  required
+                />
+              </div>
+            </div>
+            <div className={styles.formRow}>
+              <div className={styles.formGroup}>
+                <label htmlFor="hours">Horas:</label>
+                <input type="number" id="hours" name="hours" min="0" required />
               </div>
 
               <div className={styles.formGroup}>
@@ -152,21 +175,26 @@ const MiPath = () => {
                   required
                 />
               </div>
+            </div>
 
-              <div className={styles.formGroup}>
-                <label htmlFor="state">Estado:</label>
-                <select id="state" name="state" required>
-                  <option value="pendiente">Pendiente</option>
-                  <option value="en_progreso">En Progreso</option>
-                  <option value="completada">Completada</option>
-                </select>
-              </div>
+            {/* <div className={styles.formGroup}>
+              <label htmlFor="state">Estado:</label>
+              <select id="state" name="state" required>
+                <option value="pendiente">Pendiente</option>
+                <option value="en_progreso">En Progreso</option>
+                <option value="completada">Completada</option>
+              </select>
+            </div> */}
 
-              <button type="submit" className={styles.button}>
+            <div className={styles.formGroup}>
+              <button
+                type="submit"
+                className={`${styles.button} dark-gradient-primary`}
+              >
                 Añadir Actividad
               </button>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </>
     );
